341b4680e21dbbdf9d9ad52e421cfe9038c6144c
DanOsad <dan@osadtsuk.com>
2022-07-28 16:44:10 +0300

    added state values to meme.js

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,37 @@
-import React from "react"
+import React, {useState} from "react"
+import memesData from "../memesData"
 
 export default function Meme() {
+    const getRandomMemeUrl = () => {
+        const memeArray = memesData.data.memes
+        const randomIndex = Math.floor(Math.random() * memeArray.length)
+        setMemeImage(memeArray[randomIndex].url)
+    }
+
+    /* SET STATE */
+    const [memeImage, setMemeImage] = useState("")
+
     return (
         <div className='form'>
             <div className='form-input'>
-                <input type='text' className='form-text' placeholder='Top Text'></input>
-                <input type='text' className='form-text' placeholder='Bottom Text'></input>
+                <input 
+                    type='text' 
+                    className='form-text' 
+                    placeholder='Top Text' 
+                />
+                <input 
+                    type='text' 
+                    className='form-text' 
+                    placeholder='Bottom Text' 
+                />
             </div>
-            <button type='submit' className='form-btn'>Get a new meme image  🖼</button>
+            <button 
+                className='form-btn'
+                onClick={getRandomMemeUrl}
+            >
+                Get a new meme image  🖼
+            </button>
+            <img src={memeImage} alt='meme' className="meme-img" />
         </div>
     )
 }
